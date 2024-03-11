da56e289c1292fd1c0414f106cab5318057a2610
Jose <dstryr77@gmail.com>
2024-03-11 13:46:34 -0500

    cahnge connection to pool

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -1,5 +1,7 @@
 import { connect } from "mongoose";
+import { UserModel } from "schemas/User";
 
 export async function ConnectDB() {
-    await connect(process.env.MONGO_URl||'')
+    await connect(process.env.MONGO_URl||'',{minPoolSize:2})
+    await UserModel.find({})
 }
