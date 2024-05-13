88d83848999a35f51dc894566858cc54c38e1a1e
Jose <dstryr77@gmail.com>
2024-05-13 09:23:53 -0500

    change bgcolor

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -28,16 +28,16 @@ export function Profile() {
         <section className="flex flex-col items-center h-screen gap-20 bg-black/60 lg:ml-80">
             
             <SideBar/>
-            <main className="flex flex-col justify-evenly items-start min-w-80 min-h-80 mx-auto mt-20 bg-black/80 rounded-lg text-white/80 pl-10 lg:h-full">
+            <main className="flex flex-col items-start min-w-80 min-h-80 mx-auto mt-20 bg-[#212123]  rounded-lg text-white/80 pl-10 lg:h-full">
                 <div className="mx-auto ">
                     <img src={`${user?.image?user.image:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png"}`} alt=""  className="w-16 rounded-full"/>
                 </div>
-                <div className="">
-                    <h1 className="hover:cursor-pointer hover:bg-white/20">Username:  {user.username.toLocaleUpperCase()}</h1>
-                    <h2  className="hover:cursor-pointer hover:bg-white/20">Email:  {user.email}</h2>
-                    <h1 className="hover:cursor-pointer hover:bg-white/20">Address:  {user.address?.city ||'there isnt city'}</h1>
-                    <h1 className="hover:cursor-pointer hover:bg-white/20">State:  {user.address?.state ||'there isnt state'}</h1>
-                    <h1 className="hover:cursor-pointer hover:bg-white/20">Street:  {user.address?.street ||'there isnt street'}</h1>
+                <div className="flex flex-col gap-2 ">
+                    <h1 className="hover:cursor-pointer hover:bg-white/20 rounded-xl px-2 py-1">Username:  {user.username.toLocaleUpperCase()}</h1>
+                    <h2  className="hover:cursor-pointer hover:bg-white/20 rounded-xl px-2 py-1">Email:  {user.email}</h2>
+                    <h1 className="hover:cursor-pointer hover:bg-white/20 rounded-xl px-2 py-1">Address:  {user.address?.city ||'there isnt city'}</h1>
+                    <h1 className="hover:cursor-pointer hover:bg-white/20 rounded-xl px-2 py-1">State:  {user.address?.state ||'there isnt state'}</h1>
+                    <h1 className="hover:cursor-pointer hover:bg-white/20 rounded-xl px-2 py-1">Street:  {user.address?.street ||'there isnt street'}</h1>
                 </div>
             </main>
            
