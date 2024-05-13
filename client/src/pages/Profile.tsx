80ea56e729e7d3ca9cc98fe4515b55b5c5871340
Jose <dstryr77@gmail.com>
2024-05-13 09:26:18 -0500

    update css

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -28,11 +28,11 @@ export function Profile() {
         <section className="flex flex-col items-center h-screen gap-20 bg-black/60 lg:ml-80">
             
             <SideBar/>
-            <main className="flex flex-col items-start min-w-80 min-h-80 mx-auto mt-20 bg-[#212123]  rounded-lg text-white/80 pl-10 lg:h-full">
+            <main className="flex flex-col items-start min-w-80 min-h-80 mx-auto bg-[#212123] rounded-lg text-white/80 pl-10 h-full w-full py-10 gap-y-10">
                 <div className="mx-auto ">
                     <img src={`${user?.image?user.image:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png"}`} alt=""  className="w-16 rounded-full"/>
                 </div>
-                <div className="flex flex-col gap-2 ">
+                <div className="flex flex-col gap-2 mx-auto">
                     <h1 className="hover:cursor-pointer hover:bg-white/20 rounded-xl px-2 py-1">Username:  {user.username.toLocaleUpperCase()}</h1>
                     <h2  className="hover:cursor-pointer hover:bg-white/20 rounded-xl px-2 py-1">Email:  {user.email}</h2>
                     <h1 className="hover:cursor-pointer hover:bg-white/20 rounded-xl px-2 py-1">Address:  {user.address?.city ||'there isnt city'}</h1>
