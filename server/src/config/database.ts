ce6881e1b505c39f7dc8d15937008049d24d1e59
Jw0fss <dstryr77@gmail.com>
2025-07-18 22:48:03 -0500

    Fix typo in MONGO_URL variable in database connection

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -4,5 +4,5 @@ import mongoose, { connect } from "mongoose";
 export const db = mongoose.connection;
 
 export async function ConnectDB() {
-    await connect(process.env.MONGO_URl||'',{minPoolSize:5,maxPoolSize:15,socketTimeoutMS:3600000})
+    await connect(process.env.MONGO_URL||'',{minPoolSize:5,maxPoolSize:15,socketTimeoutMS:3600000})
 }
